656c43d2961ec5a29a2a0b166048ecd553d263cc
Стас Басов <sofbai25@gmail.com>
2022-05-13 14:39:00 +0300

    2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
